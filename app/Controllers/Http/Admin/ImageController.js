47cab6e150cf142014001a76c849b60d7245e89f
jorgitodf <jspaiva.1977@gmail.com>
2019-11-17 10:36:52 -0300

    Controller Image

--- app/Controllers/Http/Admin/ImageController.js
+++ app/Controllers/Http/Admin/ImageController.js
@@ -4,6 +4,12 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const Image = use('App/Models/Image')
+const { manage_single_upload, manage_multiple_uploads } = use('App/Helpers')
+const fs = use('fs')
+const Transformer = use('App/Transformers/Admin/ImageTransformer')
+const Helpers = use('Helpers')
+
 /**
  * Resourceful controller for interacting with images
  */
@@ -17,19 +23,10 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new image.
-   * GET images/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index({ response, pagination, transform }) {
+    var images = await Image.query().orderBy('id', 'DESC').paginate(pagination.page, pagination.limit)
+    images = await transform.paginate(images, Transformer)
+    return response.send(images)
   }
 
   /**
@@ -40,7 +37,61 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
+  async store({ request, response, transform }) {
+    try {
+      // captura uma image ou mais do request
+      const fileJar = request.file('images', {
+        types: ['image'],
+        size: '2mb'
+      })
+
+      // retorno pro usuário
+      let images = []
+      // caso seja um unico arquivo - manage_single_upload
+      if (!fileJar.files) {
+        const file = await manage_single_upload(fileJar)
+        if (file.moved()) {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+
+          // transnformação
+          const transformedImage = await transform.item(image, Transformer)
+
+          images.push(transformedImage)
+
+          return response.status(201).send({ successes: images, errors: {} })
+        }
+
+        return response.status(400).send({
+          message: 'Não foi possível processar esta imagem no momento!'
+        })
+      }
+      // caso sejam vários arquivos - manage_multiple_uploads
+      let files = await manage_multiple_uploads(fileJar)
+
+      await Promise.all(
+        files.successes.map(async file => {
+          const image = await Image.create({
+            path: file.fileName,
+            size: file.size,
+            original_name: file.clientName,
+            extension: file.subtype
+          })
+          const transformedImage = await transform.item(image, Transformer)
+          images.push(transformedImage)
+        })
+      )
+
+      return response.status(201).send({ successes: images, errors: files.errors })
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível processar a sua solicitação!'
+      })
+    }
   }
 
   /**
@@ -52,19 +103,10 @@ class ImageController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing image.
-   * GET images/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show({ params: { id }, request, response, transform }) {
+    var image = await Image.findOrFail(id)
+    image = transform.item(image, Transformer)
+    return response.send(image)
   }
 
   /**
@@ -75,7 +117,18 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update({ params: { id }, request, response, transform }) {
+    var image = await Image.findOrFail(id)
+    try {
+      image.merge(request.only(['original_name']))
+      await image.save()
+      image = await transform.item(image, Transformer)
+      return response.status(200).send(image)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível atualizar esta imagem no momento!'
+      })
+    }
   }
 
   /**
@@ -86,7 +139,19 @@ class ImageController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params: { id }, request, response }) {
+    const image = await Image.findOrFail(id)
+    try {
+      let filepath = Helpers.publicPath(`uploads/${image.path}`)
+
+      fs.unlinkSync(filepath)
+      await image.delete()
+      return response.status(204).send()
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Não foi possível deletar a imagem no momento!'
+      })
+    }
   }
 }
 
